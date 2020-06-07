519317c6fcd9cf374f92067cfad3bdfa0a5b9b8a
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 15:57:38 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+ if block_given?
+    while i < collection.length
+      yield(collection[i])
+    end
+  end
+end
+
+
+my_each(collection) do |i|
+  puts i
 end
