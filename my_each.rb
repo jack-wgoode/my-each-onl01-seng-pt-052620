f269fe92bea423a816ae22165e024e91ce8140d8
Jack Goode <jack.w.goode@icloud.com>
2020-06-07 16:22:05 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,16 @@
 def my_each(collection)
  if block_given?
+   i = 0
     while i < collection.length
       yield(collection[i])
+      i += 1
     end
-  end
+ end
+ collection
 end
 
+collection = [1,2,3,4]
 
-my_each(collection) do |i|
-  puts i
+my_each(collection) do |number|
+  number
 end
